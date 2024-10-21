d873971b8c045da674058194c43929ceb2f0151f
AndrinRueegg <145564904+AndrinRueeggNoser@users.noreply.github.com>
2024-10-21 15:53:22 +0200

    Fixxing the look of the cards (now responsive)

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Card({ title, onClick }) {
     return (
-        <div onClick={onClick} style={{ border: '2px solid black', borderRadius: '8px', padding: '32px', textAlign: 'center', cursor: 'pointer' }}>
-            <h3 style={{ fontSize: '24px' }}>{title}</h3>
+        <div onClick={onClick} style={{ border: '2px solid black', width: '100%', borderRadius: '8%', padding: '5%', textAlign: 'center', cursor: 'pointer', margin: '20px 0' }}>
+            <h3 style={{ fontSize: '3em' }}>{title}</h3>
         </div>
     );
 }
